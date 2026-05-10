1b5e59602de06b0bd8cc2456bf2dc31792ed0433
Arch me <kishudwivedi30@gmail.com>
2026-05-10 16:55:44 +0530

    started the repl

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+import { startREPL } from "./repl.js"
+
+
 function main() {
-  console.log("Hello, world!");
+  startREPL();
 }
 
 main();
--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,24 @@
+import { createInterface } from 'node:readline';
+import { stdin, stdout } from 'node:process';
+
 export function cleanInput(input: string): string[] {
   return input.trim().toLowerCase().split(" ");
+}
+
+const replInterface = createInterface({
+  input: stdin,
+  output: stdout,
+  prompt: "Pokedex > ",
+})
+export function startREPL() {
+  replInterface.prompt()
+  replInterface.on("line", (line:any) => {
+    const cleaned = cleanInput(line)
+    if (cleaned.length === 0 || cleaned[0] === "") {
+      replInterface.prompt();
+      return;
+    }
+    console.log(`Your command was: ${cleaned[0]}`);
+    replInterface.prompt();
+  });
 }
